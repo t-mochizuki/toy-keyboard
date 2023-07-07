bf18325ca9ebff286a2e14ff9b16fca531e16e3e
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-07-07 10:04:24 +0900

    execute the script after the document has been parsed

--- docs/main.js
+++ docs/main.js
@@ -1,60 +1,58 @@
 (() => {
-  window.addEventListener("load", () => {
-    const body = document.querySelector("body");
+  const body = document.querySelector("body");
 
-    body.addEventListener("click", () => {
-      console.log("Toy Keyboard");
+  body.addEventListener("click", () => {
+    console.log("Toy Keyboard");
 
-      const context = new window.AudioContext();
+    const context = new window.AudioContext();
 
-      let keys = new Map([
-        ["s", new Key(391.995)], // G4
-        ["e", new Key(415.305)], // G#4
-        ["d", new Key(440.000)], // A4
-        ["r", new Key(466.164)], // A#4
-        ["f", new Key(493.883)], // B4
-        ["g", new Key(523.251)], // C5
-        ["y", new Key(554.365)], // C#5
-        ["h", new Key(587.330)], // D5
-        ["u", new Key(622.254)], // D#5
-        ["j", new Key(659.255)], // E5
-        ["k", new Key(698.456)], // F5
-        ["o", new Key(739.989)], // F#5
-        ["l", new Key(783.991)], // G5
-        ["p", new Key(830.609)], // G#5
-      ]);
+    let keys = new Map([
+      ["s", new Key(391.995)], // G4
+      ["e", new Key(415.305)], // G#4
+      ["d", new Key(440.000)], // A4
+      ["r", new Key(466.164)], // A#4
+      ["f", new Key(493.883)], // B4
+      ["g", new Key(523.251)], // C5
+      ["y", new Key(554.365)], // C#5
+      ["h", new Key(587.330)], // D5
+      ["u", new Key(622.254)], // D#5
+      ["j", new Key(659.255)], // E5
+      ["k", new Key(698.456)], // F5
+      ["o", new Key(739.989)], // F#5
+      ["l", new Key(783.991)], // G5
+      ["p", new Key(830.609)], // G#5
+    ]);
 
-      body.addEventListener("keydown", event => {
-        if (event.isComposing || event.repeat || event.keyCode === 229) {
-          return;
+    body.addEventListener("keydown", event => {
+      if (event.isComposing || event.repeat || event.keyCode === 229) {
+        return;
+      }
+
+      const key = event.key;
+      if (keys.has(key)) {
+        keys.get(key).start(context);
+
+        const keyElem = document.querySelector(`div.keyboard > div#${key}`);
+        keyElem.dataset.keyname = key;
+      }
+    });
+
+    body.addEventListener("keyup", event => {
+      if (event.isComposing || event.keyCode === 229) {
+        return;
+      }
+
+      const key = event.key;
+      if (keys.has(key)) {
+        const keyElem = document.querySelector(`div.keyboard > div#${key}`);
+        if (keyElem.dataset.keyname !== null) {
+          delete keyElem.dataset.keyname;
         }
 
-        const key = event.key;
-        if (keys.has(key)) {
-          keys.get(key).start(context);
-
-          const keyElem = document.querySelector(`div.keyboard > div#${key}`);
-          keyElem.dataset.keyname = key;
-        }
-      });
-
-      body.addEventListener("keyup", event => {
-        if (event.isComposing || event.keyCode === 229) {
-          return;
-        }
-
-        const key = event.key;
-        if (keys.has(key)) {
-          const keyElem = document.querySelector(`div.keyboard > div#${key}`);
-          if (keyElem.dataset.keyname !== null) {
-            delete keyElem.dataset.keyname;
-          }
-
-          keys.get(key).stop();
-        }
-      });
-    }, { once: true });
-  });
+        keys.get(key).stop();
+      }
+    });
+  }, { once: true });
 
   class Key {
     constructor(frequency) {
